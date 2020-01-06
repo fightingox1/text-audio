da591556c3969aa4d30e4cc004593bea036b0c5b
fightingox1 <44482532+fightingox1@users.noreply.github.com>
2020-01-06 14:22:20 -0500

    Create decode.py

---
+++ decode.py
@@ -0,0 +1,117 @@
+import wave
+a = wave.open('a', 'r')
+b = wave.open('b', 'r')
+c = wave.open('c', 'r')
+d = wave.open('d', 'r')
+e = wave.open('e', 'r')
+f = wave.open('f', 'r')
+g = wave.open('g', 'r')
+h = wave.open('h', 'r')
+i = wave.open('i', 'r')
+j = wave.open('j', 'r')
+k = wave.open('k', 'r')
+l = wave.open('l', 'r')
+m = wave.open('m', 'r')
+n = wave.open('n', 'r')
+o = wave.open('o', 'r')
+p = wave.open('p', 'r')
+q = wave.open('q', 'r')
+r = wave.open('r', 'r')
+s = wave.open('s', 'r')
+t = wave.open('t', 'r')
+u = wave.open('u', 'r')
+v = wave.open('v', 'r')
+w = wave.open('w', 'r')
+x = wave.open('x', 'r')
+y = wave.open('y', 'r')
+z = wave.open('z', 'r')
+space = wave.open('space', 'r')
+
+def decode(audio):
+  message = []
+  while go == "true":
+    if .readframes(audio) == .readframes(a):
+        message.append('a')
+    else:
+        if .readframes(audio) == .readframes(b):
+        message.append('b')
+        else:
+          if .readframes(audio) == .readframes(c):
+          message.append('c')
+          else:
+            if .readframes(audio) == .readframes(d):
+            message.append('d')
+            else:
+              if .readframes(audio) == .readframes(e):
+              message.append('e')
+              else:
+                if .readframes(audio) == .readframes(f):
+                message.append('f')
+                else:
+                  if .readframes(audio) == .readframes(g):
+                  message.append('g')
+                  else:
+                    if .readframes(audio) == .readframes(h):
+                    message.append('h')
+                    else:
+                      if .readframes(audio) == .readframes(i):
+                      message.append('i')
+                      else:
+                        if .readframes(audio) == .readframes(j):
+                        message.append('j')
+                        else:
+                          if .readframes(audio) == .readframes(k):
+                          message.append('k')
+                          else:
+                            if .readframes(audio) == .readframes(l):
+                            message.append('l')
+                            else:
+                              if .readframes(audio) == .readframes(m):
+                              message.append('m')
+                              else:
+                                if .readframes(audio) == .readframes(n):
+                                message.append('n')
+                                else:
+                                  if .readframes(audio) == .readframes(o):
+                                  message.append('o')
+                                  else:
+                                    if .readframes(audio) == .readframes(p):
+                                    message.append('p')
+                                    else:
+                                      if .readframes(audio) == .readframes(q):
+                                      message.append('q')
+                                      else:
+                                        if .readframes(audio) == .readframes(r):
+                                        message.append('r')
+                                        else:
+                                          if .readframes(audio) == .readframes(s):
+                                          message.append('s')
+                                          else:
+                                            if .readframes(audio) == .readframes(t):
+                                            message.append('t')
+                                            else:
+                                              if .readframes(audio) == .readframes(u):
+                                              message.append('u')
+                                              else:
+                                                if .readframes(audio) == .readframes(v):
+                                                message.append('v')
+                                                else:
+                                                  if .readframes(audio) == .readframes(w):
+                                                  message.append('w')
+                                                  else:
+                                                    if .readframes(audio) == .readframes(x):
+                                                    message.append('x')
+                                                    else:
+                                                      if .readframes(audio) == .readframes(y):
+                                                      message.append('y')
+                                                      else:
+                                                         if .readframes(audio) == .readframes(z):
+                                                         message.append('z')
+                                                         else:
+                                                           if .readframes(audio) == .readframes(space):
+                                                           message.append(' ')
+                                                           
+    if go1 == "true":
+      go =="true"
+
+
